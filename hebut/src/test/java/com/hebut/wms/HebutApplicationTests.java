1b81090c77b064a805554f1b2226b66b35b84d14
ningyv <785539058@qq.com>
2024-03-04 13:46:07 +0800

    20240304

---
+++ hebut/src/test/java/com/hebut/wms/HebutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebut.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HebutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
